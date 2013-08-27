72452ba39ac8f4b07d3872cdeb8cad360a5016ec
Morten Siebuhr <sbhr@sbhr.dk>
2013-08-27 11:44:09 +0200

    Reporters: Re-write to readable streams.

--- test/reporters/common.js
+++ test/reporters/common.js
@@ -1,4 +1,5 @@
-var assert = require('chai').assert;
+var assert = require('chai').assert,
+    stream = require('stream');
 
 /*global describe, it*/
 
@@ -9,7 +10,57 @@ function generateTests(reporterName) {
     describe('reporters/' + reporterName, function () {
         var r = require('../../lib/reporters/' + reporterName);
 
-        it('Doesn\'t return anything without errors');
-        it('Outputs something when given an error');
+        it('Doesn\'t return anything without errors', function (done) {
+            var outData = "";
+
+            // Report!
+            var out = r([], {});
+
+            out.on('data', function (d) {
+                outData += d;
+            }).on('end', function () {
+                // Check output
+                assert.equal(outData, '');
+                done();
+            });
+        });
+
+        describe('When given an error', function () {
+            var outData = "";
+
+            before(function (done) {
+                // Report!
+                r([{
+                    filename: 'some/file',
+                    line: 42,
+                    character: 9,
+                    message: 'Bam! It broke.'
+                }], {})
+                .on('data', function (d) { outData += d; })
+                .on('end', function () {done();});
+            });
+
+            it('Returns a non-empty string', function () {
+                assert.isString(outData);
+                assert.notEqual(outData, "");
+            });
+
+            it('Contains .filename somewhere', function () {
+                assert.include(outData, 'some/file');
+            });
+
+            it('Contains .message somewhere', function () {
+                assert.include(outData, 'Bam! It broke.');
+            });
+
+            it('Contains .line somewhere', function () {
+                assert.include(outData, '42');
+            });
+
+            it('Contains .character somewhere', function () {
+                assert.include(outData, '9');
+            });
+
+        });
     });
 }
