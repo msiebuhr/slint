62585674498e4b6476895cadb8d63bb5aec42969
Morten Siebuhr <sbhr@sbhr.dk>
2013-08-27 09:12:51 +0200

    Tests: Make basic common tests for both linters.
     * Add a simple common test-suite.
     * Fix trivial differences in output.

--- test/linters/common.js
+++ test/linters/common.js
@@ -3,22 +3,67 @@ var assert = require('chai').assert,
 
 /*global describe, it*/
 
-describe('linters/trailing-witespace', function () {
+var basicTests = {
+    'trailing-spaces': {
+        ok: 'bar\nbaz\n',
+        fail: 'bar \nbaz\n'
+    },
+    'jshint': {
+        ok: 'return 1+1;',
+        fail: 'return return'
+    }
+};
 
-    it('Lets OK data pass', function (done) {
-        trailingWhitespace('foo', 'bar\nbaz\n', {}, function (err, errors) {
-            assert.deepEqual(errors, []);
-            done(err);
+// Loop through basic tests
+for (var testName in basicTests) { generateTests(testName); }
+
+// Generate a test-suite for the given linter.
+function generateTests(testName) {
+    describe('linters/' + testName, function () {
+        var l = require('../../lib/linters/' + testName);
+
+        it('Does not return errors on OK data', function (done) {
+            l('fileName', basicTests[testName].ok, {}, function (err, errors) {
+                assert.isArray(errors);
+                assert.lengthOf(errors, 0);
+                done(err);
+            });
+        });
+
+        describe('Bad data', function () {
+            var errList;
+
+            before(function (done) {
+                l('fileName', basicTests[testName].fail, {}, function (err, errors) {
+                    assert.isArray(errors, 'Expected a list of errors.')
+                    errList = errors;
+                    done(err);
+                });
+            });
+
+            it('Returns at least one error', function () {
+                assert.operator(errList.length, '>', 0, 'Expected at least one error')
+            });
+
+            it('Has err.filename = fileName', function () {
+                errList.forEach(function (err) {
+                    assert.propertyVal(err, 'filename', 'fileName');
+                });
+            });
+
+            it('Has .line = <some nubmer>', function () {
+                errList.forEach(function (err) {
+                    assert.property(err, 'line');
+                    assert.isNumber(err.line);
+                });
+            });
+
+            it('Has .character = <some number>', function () {
+                errList.forEach(function (err) {
+                    assert.property(err, 'character');
+                    assert.isNumber(err.character)
+                });
+            });
         });
     });
-
-    it('Barfs on trailing spaces', function (done) {
-        trailingWhitespace('foo', 'bar \nbaz\n', {}, function (err, errors) {
-            assert.lengthOf(errors, 1);
-            assert.propertyVal(errors[0], 'line', 1);
-            assert.propertyVal(errors[0], 'char', 4);
-            done(err);
-        });
-    });
-});
-
+}
