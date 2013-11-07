bf8ca21cbe87a21e6107a00735a3eedc93e574cc
Andreas Lind Petersen <andreas@one.com>
2013-11-07 22:20:12 +0100

    npm-packages linter:
    * Avoid sync fs method
    * Allow node_modules to be missing if no dependencies are listed
    * Complain if node_modules is not a directory (or a symlink to a directory)

--- test/linters/npm-packages.js
+++ test/linters/npm-packages.js
@@ -25,4 +25,45 @@ describe('npm-packages', function () {
             });
         });
     });
+
+    describe('in a project with no dependencies listed in package.json and no node_modules folder', function () {
+        var projectDirectory = Path.resolve(__dirname, 'npm-packages', 'noDependenciesAndNoNodeModules'),
+            packageJsonPath = Path.resolve(projectDirectory, 'package.json'),
+            packageJsonStr;
+
+        before(function (done) {
+            fs.readFile(packageJsonPath, 'utf-8', function (err, contents) {
+                packageJsonStr = contents;
+                done(err);
+            });
+        });
+        it('should output zero errors', function (done) {
+            npmPackages(packageJsonPath, packageJsonStr, {}, function (err, results) {
+                assert.isArray(results);
+                assert.equal(results.length, 0);
+                done(err);
+            });
+        });
+    });
+
+    describe('in a project with a package.json that has a dependencies property with a string value', function () {
+        var projectDirectory = Path.resolve(__dirname, 'npm-packages', 'nonObjectDependencies'),
+            packageJsonPath = Path.resolve(projectDirectory, 'package.json'),
+            packageJsonStr;
+
+        before(function (done) {
+            fs.readFile(packageJsonPath, 'utf-8', function (err, contents) {
+                packageJsonStr = contents;
+                done(err);
+            });
+        });
+        it('should output one error', function (done) {
+            npmPackages(packageJsonPath, packageJsonStr, {}, function (err, results) {
+                assert.isArray(results);
+                assert.equal(results.length, 1);
+                assert.equal(results[0].message, 'The dependencies property should be an object if present');
+                done(err);
+            });
+        });
+    });
 });
